2d2e984b038279bafe062eb1c9214a2997830316
yunisrasulzade <yunisrasulzada99@gmail.com>
2020-07-03 11:43:41 +0400

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = "yunis";
+    MotionLayout motionLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
